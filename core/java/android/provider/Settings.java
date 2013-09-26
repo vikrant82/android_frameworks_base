43373afdefbd30dc8a766b12ac942e61173a09af
htchoi1995 <htchoi1995@gmail.com>
2013-09-26 13:01:58 +0200

    [1/2] SystemUI: Network speed indicator
    Change-Id: I88e7cdfe20342c695a33383db36bf80bb4ffe009
    
    Conflicts:
    	packages/SystemUI/res/layout/status_bar.xml
    
    Conflicts:
    	packages/SystemUI/res/layout/status_bar.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3573,6 +3573,11 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_COLOR = "network_traffic_color";
 
+	    /** Network speed indicator
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
