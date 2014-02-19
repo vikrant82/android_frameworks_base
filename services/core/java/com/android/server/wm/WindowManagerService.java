10741f53c045330760d899e8f29611df8aa552e6
Lars Greiss <kufikugel@googlemail.com>
2014-02-19 07:43:38 +0100

    Recents Slim
    Change-Id: I0555dceaf511e535a301ed6283e67d148eb7a9b5

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -12090,4 +12090,10 @@ public class WindowManagerService extends IWindowManager.Stub
             }
         }
     }
+
+    /* @hide */
+    @Override
+    public int getSystemUIVisibility() {
+        return mLastStatusBarVisibility;
+    }
 }
